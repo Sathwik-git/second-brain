8f0cb0b61da64b2cd8390e9c3a27cfdb8bcbc1eb
Sathwik-git <sathwikbodakuntla041@gmail.com>
2024-12-18 23:12:07 +0530

    added userMiddleWare

--- src/index.ts
+++ src/index.ts
@@ -49,17 +49,20 @@ app.post("/api/v1/signin", async (req: Request, res: Response) => {
       res.json({ msg: "Enter valid credentials" });
       return;
     }
+
     if (!process.env.JWT_SECRET) {
       throw new Error("JWT_SECRET is not defined in the .env file");
     }
-    const token = jwt.sign(req.body, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     res.json({ msg: "successful signin", token: token });
   } catch (error) {
     res.status(500).json({ msg: "Server error", error });
   }
 });
 
-app.post("/api/v1/content", (req, res) => {});
+app.post("/api/v1/content", (req, res) => {
+  
+});
 
 app.get("/api/v1/content", (req, res) => {});
 
---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const userMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+  const decode = jwt.verify(header as string, process.env.JWT_SECRET as string);
+  if (decode) {
+    req.body.userId = (decode as JwtPayload).id;
+    next();
+  }else{
+    res.json({msg:"Access Denied"})
+  }
+};
